59d81e3a43febcd8cb7afa3f5d5a4849ba09d91e
Dmitry <dimos9813@gmail.com>
2023-09-25 22:27:05 +0800

    Add redux store, add first entity

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,5 @@
 import { type FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Counter } from 'entities/Counter'
 
 const MainPage: FC = () => {
     const { t } = useTranslation()
