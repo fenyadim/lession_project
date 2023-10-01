e9aaee525aeda5d4406602f0c8149961dc84881c
Dmitry <dimos9813@gmail.com>
2023-10-01 23:52:59 +0800

    Add login module

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src')
     }
-    config.resolve.modules.push(paths.src)
+    config.resolve.modules = [paths.src, 'node_modules']
     config.resolve.extensions.push('.ts', '.tsx')
 
     config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
