02a4b7301a17ee0df255e0b24c34f99d914fc10a
fenyadim <dimos9813@gmai.com>
2023-11-05 16:54:04 +0800

    add filter, sort, search

--- src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
+++ src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
@@ -4,7 +4,7 @@ import { getArticlePageInited } from '../../selectors/articlesPageSelectors'
 import { articlePageAction } from '../../slices/articlesPageSlice'
 import { fetchArticleList } from '../../services/fetchArticleList/fetchArticleList'
 import { type SortOrder } from 'shared/types'
-import { type ArticleSortField } from 'entities/Article'
+import { type ArticleSortField, type ArticleType } from 'entities/Article'
 
 // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
 export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkConfig<string>>(
@@ -17,6 +17,7 @@ export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkCon
             const orderFromUrl = searchParams.get('order') as SortOrder
             const sortFromUrl = searchParams.get('sort') as ArticleSortField
             const searchFromUrl = searchParams.get('search')
+            const typeFromUrl = searchParams.get('type') as ArticleType
 
             if (orderFromUrl) {
                 void dispatch(articlePageAction.setOrder(orderFromUrl))
@@ -26,6 +27,9 @@ export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkCon
             }
             if (searchFromUrl) {
                 void dispatch(articlePageAction.setSearch(searchFromUrl))
+            }
+            if (typeFromUrl) {
+                void dispatch(articlePageAction.setType(typeFromUrl))
             }
 
             void dispatch(articlePageAction.initState())
