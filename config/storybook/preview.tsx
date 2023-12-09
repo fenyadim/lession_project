d74377b7fcb6aeeec1c7cd093689b84410808c41
fenyadim <dimos9813@gmail.com>
2023-12-09 23:32:58 +0800

    Add theme switcher in storybook

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -2,12 +2,10 @@ import { type Preview } from '@storybook/react'
 import { RouterDecorator } from '@/6_shared/config/storybook/RouterDecorator'
 import { StyleDecorator } from '@/6_shared/config/storybook/StyleDecorator'
 import { SuspenseDecorator } from '@/6_shared/config/storybook/SuspenseDecorator'
-import { ThemeDecorator } from '@/6_shared/config/storybook/ThemeDecorator'
 import { Theme } from '@/6_shared/const/theme'
 
 const preview: Preview = {
     decorators: [
-        ThemeDecorator(Theme.LIGHT),
         StyleDecorator,
         RouterDecorator,
         SuspenseDecorator
@@ -19,6 +17,13 @@ const preview: Preview = {
                 color: /(background|color)$/i,
                 date: /Date$/
             }
+        },
+        themes: {
+            default: 'light',
+            list: [
+                { name: 'light', class: Theme.LIGHT, color: '#FAEBD7FF' },
+                { name: 'dark', class: Theme.DARK, color: '#4169E1FF' }
+            ]
         }
     }
 }
